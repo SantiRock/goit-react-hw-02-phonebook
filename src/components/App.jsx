e55f7e518a35b95b38f8987665961c9a6064fa8b
Santiago Quintero <santiago.qp86@gmail.com>
2023-04-24 20:39:18 -0500

    test3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,78 @@
 import { nanoid } from 'nanoid';
 import { Component } from 'react';
-import { ContactForm } from './ContactForm';
-import { ContactList } from './ContactList';
-import { Filter } from './Filter';
+import PropTypes from "prop-types";
 import css from './App.module.css'
+
+const ContactForm = ( {handleSubmit, handleChange} ) => {
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <p className={css.p}>Name</p>
+      <input 
+      className={css.input} 
+      type='text' 
+      name='name'
+      onChange={handleChange}
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+      />
+      <p className={css.p}>Number</p>
+      <input 
+      className={css.input} 
+      type='tel' 
+      name='number'
+      onChange={handleChange}
+      pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+      required
+      />
+      <button className={css.btn} type='submit'>Add contact</button>
+    </form>
+  )
+}
+
+ContactForm.propTypes = {
+handleSubmit: PropTypes.func,
+handleChange: PropTypes.func,
+}
+
+const ContactList = ( { arr, handleDelete} ) => {
+  return (
+    <ul>
+      {arr.map( person => (
+        <li className={css.li} key={person.id}>
+          {person.name}: {person.number}
+          <button className={css.btndel} onClick={() => handleDelete(person.id)}>Delete</button>
+        </li>
+
+      ))}
+    </ul> 
+  )
+}
+
+ContactList.propTypes = {
+  arr: PropTypes.array,
+  handleDelete: PropTypes.func
+
+}
+
+const Filter = ( { handleFilter } ) => {
+  return(
+    <>
+      <p className={css.p}>Find contacts by name</p>
+      <input 
+      onChange={handleFilter}
+      type='text'
+      name='filter'
+      />
+    </>
+  )
+}
+
+Filter.propTypes = {
+handleFilter: PropTypes.func,
+}
+
 
 class App extends Component {
     state = {
